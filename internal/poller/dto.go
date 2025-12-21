63f67f6797996a31675019d0ac2258bc658bc68b
WoozyMasta <kagbe.leviy@gmail.com>
2025-12-21 19:32:10 +0300

    fix: rcon poller append

--- internal/poller/dto.go
+++ internal/poller/dto.go
@@ -5,26 +5,29 @@ import dto "github.com/prometheus/client_model/go"
 // Helper to create DTO structures cleanly
 func addGauge(families map[string]*dto.MetricFamily, name, help string, value float64, instanceID string) {
 	labelName := "instance_id"
-
-	mf := &dto.MetricFamily{
-		Name: &name,
-		Help: &help,
-		Type: dto.MetricType_GAUGE.Enum(),
-		Metric: []*dto.Metric{
+	metric := &dto.Metric{
+		Label: []*dto.LabelPair{
 			{
-				Label: []*dto.LabelPair{
-					{
-						Name:  &labelName,
-						Value: &instanceID,
-					},
-				},
-				Gauge: &dto.Gauge{
-					Value: &value,
-				},
+				Name:  &labelName,
+				Value: &instanceID,
 			},
 		},
+		Gauge: &dto.Gauge{
+			Value: &value,
+		},
 	}
-	families[name] = mf
+
+	// Create new series or add to exists
+	if mf, ok := families[name]; ok {
+		mf.Metric = append(mf.Metric, metric)
+	} else {
+		families[name] = &dto.MetricFamily{
+			Name:   &name,
+			Help:   &help,
+			Type:   dto.MetricType_GAUGE.Enum(),
+			Metric: []*dto.Metric{metric},
+		}
+	}
 }
 
 func addGaugeWithLabels(families map[string]*dto.MetricFamily, name, help string, value float64, labels map[string]string) {
@@ -38,16 +41,20 @@ func addGaugeWithLabels(families map[string]*dto.MetricFamily, name, help string
 		})
 	}
 
-	mf := &dto.MetricFamily{
-		Name: &name,
-		Help: &help,
-		Type: dto.MetricType_GAUGE.Enum(),
-		Metric: []*dto.Metric{
-			{
-				Label: labelPairs,
-				Gauge: &dto.Gauge{Value: &value},
-			},
-		},
+	metric := &dto.Metric{
+		Label: labelPairs,
+		Gauge: &dto.Gauge{Value: &value},
 	}
-	families[name] = mf
+
+	// Create new series or add to exists
+	if mf, ok := families[name]; ok {
+		mf.Metric = append(mf.Metric, metric)
+	} else {
+		families[name] = &dto.MetricFamily{
+			Name:   &name,
+			Help:   &help,
+			Type:   dto.MetricType_GAUGE.Enum(),
+			Metric: []*dto.Metric{metric},
+		}
+	}
 }
